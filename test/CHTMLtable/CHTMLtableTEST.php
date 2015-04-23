8f7c87876fbc502c19f1dcf86f76cc9fb68dd807
KriRin <klinsul91@gmail.com>
2015-04-23 20:56:45 +0200

    trying phpunit

--- test/CHTMLtable/CHTMLtableTEST.php
+++ test/CHTMLtable/CHTMLtableTEST.php
@@ -2,25 +2,68 @@
 
 namespace Kri\HTMLtable;
 
+
 class CHTMLtableTest extends \PHPUnit_Framework_TestCase {
 
 
+
 public function testgetTable() {
-    $obj = new \Kri\HTMLtable\CHTMLtable();
+    $table = new \Kri\HTMLtable\CHTMLtable();
     
     $td = [ '1' => [ 'name' => 'John']];
-    $td = [ '1' => 'name'];
+    $th = [ '1' => 'name'];
     
-    $res = $obj->getTable($td, $th);
+    $res = $table->getTable($td, $th);
     
-    $exp = '<table><tr><th>Name</th></tr><tr><td>John</td></tr></table>';
+    $exp = '<table><tr><th>name</th></tr><tr><td>John</td></tr></table>';
     
-    $this->assertEquals($res, $exp, "Created table missmatch when using array as data in method getTable().");
+    $this->assertEquals($res, $exp, "Created table missmatch when inserting td and th value in method getTable().");
     
-
 }
 
 
+
+/**
+public function testgetTablePagering() {
+    $obj = new \Kri\HTMLtable\CHTMLtable();
+    
+    $td = ['ex1' => [
+                        'name' => 'Adam',
+                        'age' => '18',
+                        'occupation' => 'studying',
+                        'adress' => 'abcroad 123',
+                        ],
+                'ex2' => [
+                        'name' => 'Bob',
+                        'age' => '22',
+                        'occupation' => 'webb dev',
+                        'adress' => 'abcroad 321',
+                        ],
+                'ex3' => [
+                        'name' => 'test1',
+                        'age' => 'test2',
+                        ],
+                'ex4',
+                'ex5',
+                'ex6',
+                'ex7' => [
+                        'name' => 'John',
+                        'age' => '??',
+                        'occupation' => 'admin',
+                        'adress' => 'Doestreet 1',
+                        ],
+                'ex4',
+                'ex5',
+                'ex6',
+                'ex7',
+                ];
+    $th = [ 0 => "Name", "Age", "Occupation", "Adress"];
+    $data = $obj->getTable($td, $th, true, 3);
+    $exp = "<table><tr><th>Name</th><th>Age</th><th>Occupation</th><th>Adress</th></tr><tr><td>Adam</td><td>18</td><td>studying</td><td>abcroad 123</td></tr><tr><td>Bob</td><td>22</td><td>webb dev</td><td>abcroad 321</td></tr><tr><td>test1</td><td>test2</td></tr></table>&lt;&lt; &lt; 1 <a href='?page=2'>2</a> <a href='?page=3'>3</a> <a href='?page=4'>4</a> <a href='?page=2'>&gt;</a> <a href='?page=4'>&gt;&gt;</a>";
+
+    $this->assertEquals($data, $exp, "Created table missmatch when enabling pagering in method getTable().");
+}
+**/
 
 
 
